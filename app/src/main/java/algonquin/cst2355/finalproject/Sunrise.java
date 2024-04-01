b44e47af969b23887ea16d6c030e8db12069d7dc
krish <krishp140405@gmail.com>
2024-04-01 11:30:22 -0400

    commit 1

--- app/src/main/java/algonquin/cst2355/finalproject/Sunrise.java
+++ app/src/main/java/algonquin/cst2355/finalproject/Sunrise.java
@@ -222,30 +222,7 @@ public class Sunrise extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.help) {
-            showHelpDialog();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void showHelpDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(getString(R.string.help_title));
-        builder.setMessage(getString(R.string.help_content));
-        builder.setPositiveButton("OK", null);
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
 
     private void lookupSunriseSunset(String selectedLat, String selectedLng) {
     }
