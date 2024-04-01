54b43248f8a0de1aa3867721b92f146e9e1fab42
Harnoor Singh <sing1977@algonquincollege.com>
2024-04-01 09:48:36 -0400

    added recipe class

---
+++ app/src/main/java/algonquin/cst2355/finalproject/Recipe.java
@@ -0,0 +1,38 @@
+package algonquin.cst2355.finalproject;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "recipe_table")
+public class Recipe {
+
+    private String title;
+    private String imageUrl;
+    private String summary;
+    private String sourceUrl;
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    public Recipe(int id,String title, String imageUrl) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public String getSummary()
+    { return summary; }
+    public String getSourceUrl()
+    { return sourceUrl; }
+    public int getId()
+    { return id; }
+    public void setSummary(String summary)
+    { this.summary = summary; }
+    public void setSourceUrl
+            (String sourceUrl) { this.sourceUrl = sourceUrl; }
+}
